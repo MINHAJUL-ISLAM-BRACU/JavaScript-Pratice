20821f633e7059a426cd3cd7fcc0805ed167c11c
MINHAJUL ISLAM <minhajul_islam1@hotmail.com>
2024-07-08 17:58:50 +0600

    Today Task

---
+++ practice.js
@@ -0,0 +1,22 @@
+var studyGroup=10;
+console.log(studyGroup)
+
+let x=0;
+let y=8;
+
+
+let z = myFunction(4, 3);
+console.log(z)
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+
+let person ={
+    name:"Meee",
+    age:"30",
+    city:"New York"
+};
+
+console.log(JSON.stringify(person))
